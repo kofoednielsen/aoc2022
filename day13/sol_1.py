32d051959ce5ec7e673ee8ad851ff520316cb158
Asbjørn Kofoed-Nielsen <askn@jobindex.dk>
2022-12-13 20:12:52 +0100

    aaaah 🧠

--- day13/sol_1.py
+++ day13/sol_1.py
@@ -1,5 +1,5 @@
 import json
-lines = open('sample.txt').read().split('\n')
+lines = open('input.txt').read().split('\n')
 
 pairs = []
 pair = []
@@ -9,25 +9,23 @@ for line in lines:
         pair = []
     else:
         pair.append(json.loads(line))
-
-for pair in pairs:
-    print(pair)
-
+    
 def is_in_order(pair):
     # cast to lists
-    print(f"comparing {pair[0]}, {pair[1]}")
     for x, y in zip(*pair):
         if all(isinstance(v, int) for v in [x,y]):
-            print(f"comparing {x}, {y}")
             if x < y:
-                print(f"✔️ {x} was smaller {y}")
                 return True
             if x > y:
-                print(f"❌ {x} was larger {y}")
                 return False
-        elif not is_in_order([v if isinstance(v, list) else [v] for v in [x,y]]):
-                return False
-    return len(pair[0]) <= len(pair[1])
+        else:
+            in_order = is_in_order([v if isinstance(v, list) else [v] for v in [x,y]])
+            if in_order != None:
+                return in_order
+    if len(pair[0]) < len(pair[1]):
+        return True
+    if len(pair[0]) > len(pair[1]):
+        return False
 
 
-print([i+1 for i, _ in filter(lambda tup: is_in_order(tup[1]), enumerate(pairs))])
+print(sum([i+1 for i, _ in filter(lambda tup: is_in_order(tup[1]), enumerate(pairs))]))
